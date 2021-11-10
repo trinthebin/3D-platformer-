bc530a9abeb018c595568c9fb5b7444fc50022bf
Trinity Rosales <trosales2021@elam.co.uk>
2021-11-10 10:33:30 +0000

    i worked on the coding in this lesson

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    public float maxSpeed = 3.0f;
+    public float maxSpeed;
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+
+
     float rotation = 0.0f;
     float camRotation = 0.0f;
     float rotationSpeed = 2.0f;
     public float camRotationSpeed = 1.5f;
+    public float jumpForce = 300.0f;
 
     Rigidbody myRigidbody;
-
     GameObject cam;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +32,16 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
 
-        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+
+
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed)
         myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
