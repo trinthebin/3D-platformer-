758026f09c87821686c97f8aa2f8db9402466aa3
Trinity Rosales <trosales2021@elam.co.uk>
2021-11-17 09:55:11 +0000

    added pickup code

--- Assets/PickUp.cs
+++ Assets/PickUp.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Destroy(gameObject);
     }
 }
